97b214ed9243e6778c5316c72a95f3f50b79d3d7
Yuri Weilemann <t2m.yweilemann@terceiros.mag.com.br>
2020-08-12 14:35:42 -0300

    Amazon SES configured

---
+++ iniciando-back-end/src/config/mail.ts
@@ -0,0 +1,20 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'INSERIR ALGUM EMAIL QUE EU CADASTRAR NO DOMINIO',
+      name: 'Yuri da GoBarber',
+    },
+  },
+} as IMailConfig;
