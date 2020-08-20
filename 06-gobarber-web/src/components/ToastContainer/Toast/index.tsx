1b0a6615ab64d09e5016272e17f6eb6435eaacb0
Yuri Weilemann <t2m.yweilemann@terceiros.mag.com.br>
2020-08-20 20:04:08 -0300

    Forgot Password screen created

--- 06-gobarber-web/src/components/ToastContainer/Toast/index.tsx
+++ 06-gobarber-web/src/components/ToastContainer/Toast/index.tsx
@@ -12,6 +12,7 @@ import { Container } from './styles';
 
 interface ToastProps {
   message: ToastMessage;
+  // eslint-disable-next-line @typescript-eslint/ban-types
   style: object;
 }
 
@@ -37,7 +38,7 @@ const Toast: React.FC<ToastProps> = ({ message, style }) => {
   return (
     <Container
       type={message.type}
-      hasDescription={!!message.description}
+      hasDescription={Number(!!message.description)}
       style={style}
     >
       {icons[message.type || 'info']}
